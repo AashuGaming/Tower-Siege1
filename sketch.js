9f186d7d243adb82432b7ed68922978fbdb07b74
AashuGaming <77149352+AashuGaming@users.noreply.github.com>
2021-01-09 14:34:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,118 @@
+
+var ball;
+var block1, block2;
+var brick1, brick2, brick3, brick4, brick5, brick6, brick7, brick8, brick9, brick10, brick11, brick12, brick13, brick14, brick15, brick16, brick17, brick18, brick19, brick20, brick21, brick22, brick23, brick24, brick25, brick26, brick27, brick28, brick29, brick30
+
+function setup() {
+  createCanvas(1600, 700)
+
+  ball = createSprite(200, 400, 25, 25);
+  ball.shapeColor = "yellow";
+
+  block1 = createSprite(600, 500, 250, 15);
+  block1.shapeColor = "orange";
+
+  brick1 = createSprite(500, 480, 25, 25);
+  brick1.shapeColor =  "lightblue";
+
+  brick2 = createSprite(530, 480, 25, 25);
+  brick2.shapeColor =  "lightblue";
+
+  brick3 = createSprite(560, 480, 25, 25);
+  brick3.shapeColor =  "lightblue";
+
+  brick4 = createSprite(590, 480, 25, 25);
+  brick4.shapeColor =  "lightblue";
+
+  brick5 = createSprite(620, 480, 25, 25);
+  brick5.shapeColor =  "lightblue";
+
+  brick6 = createSprite(650, 480, 25, 25);
+  brick6.shapeColor =  "lightblue";
+
+  brick7 = createSprite(680, 480, 25, 25);
+  brick7.shapeColor =  "lightblue";
+
+  brick8 = createSprite(530, 450, 25, 25);
+  brick8.shapeColor =  "lightblue";
+
+  brick9 = createSprite(560, 450, 25, 25);
+  brick9.shapeColor =  "lightblue";
+
+  brick10 = createSprite(590, 450, 25, 25);
+  brick10.shapeColor =  "lightblue";
+
+  brick11 = createSprite(620, 450, 25, 25);
+  brick11.shapeColor =  "lightblue";
+
+  brick12 = createSprite(650, 450, 25, 25);
+  brick12.shapeColor =  "lightblue";
+
+  brick13 = createSprite(560, 420, 25, 25);
+  brick13.shapeColor =  "lightblue";
+
+  brick14 = createSprite(590, 420, 25, 25);
+  brick14.shapeColor =  "lightblue";
+
+  brick15 = createSprite(620, 420, 25, 25);
+  brick15.shapeColor =  "lightblue";
+
+  brick16 = createSprite(590, 390, 25, 25);
+  brick16.shapeColor =  "lightblue";
+
+  brick17 = createSprite(930, 380, 25, 25);
+  brick17.shapeColor =  "lightblue";
+
+  brick18 = createSprite(960, 380, 25, 25);
+  brick18.shapeColor =  "lightblue";
+
+  brick19 = createSprite(990, 380, 25, 25);
+  brick19.shapeColor =  "lightblue";
+
+  brick20 = createSprite(1020, 380, 25, 25);
+  brick20.shapeColor =  "lightblue";
+
+  brick21 = createSprite(1050, 380, 25, 25);
+  brick21.shapeColor =  "lightblue";
+
+  brick22 = createSprite(1080, 380, 25, 25);
+  brick22.shapeColor =  "lightblue";
+
+  brick23 = createSprite(1050, 350, 25, 25);
+  brick23.shapeColor =  "lightblue";
+
+  brick24 = createSprite(1020, 350, 25, 25);
+  brick24.shapeColor =  "lightblue";
+
+  brick25 = createSprite(990, 350, 25, 25);
+  brick25.shapeColor =  "lightblue";
+
+  brick26 = createSprite(960, 350, 25, 25);
+  brick26.shapeColor =  "lightblue";
+
+  brick27 = createSprite(990, 320, 25, 25);
+  brick27.shapeColor =  "lightblue";
+
+  brick28 = createSprite(1020, 320, 25, 25);
+  brick28.shapeColor =  "lightblue";
+
+  brick29 = createSprite(1001, 290, 25, 25);
+  brick29.shapeColor =  "lightblue";
+
+
+
+
+
+
+
+  block2 = createSprite(1000, 400, 250, 15);
+  block2.shapeColor = "orange";
+
+
+
+}
+
+function draw() {
+  background("brown")
+  drawSprites();
+}
